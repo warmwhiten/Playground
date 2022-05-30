5f24c9da39dfd09ce0fd661a5a45e7e708b2e737
warmwhiten <ekekekgml@naver.com>
2022-05-30 21:06:04 +0900

    setup dev environment for storybook

---
+++ packages/common-components/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from './stories/Button.jsx'
+
+const Header = ({ text = '' }) => {
+  return <h1>Shared header library {text}</h1>
+}
+
+export { Button, Header }
---
+++ packages/common-components/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: { import: './src/index.js' }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  output: {
+    filename: 'components.bundle.min.js',
+    library: 'fstrComponents',
+    libraryTarget: 'umd',
+    clean: true
+  }
+}
