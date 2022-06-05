280d0ae38fb5f18aa882726c02b33f98ac457e84
warmwhiten <ekekekgml@naver.com>
2022-06-06 03:23:16 +0900

    feat: add roulette wheel component

--- packages/common-components/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Button } from './stories/Button.jsx'
-
-const Header = ({ text = '' }) => {
-  return <h1>Shared header library {text}</h1>
-}
-
-export { Button, Header }
