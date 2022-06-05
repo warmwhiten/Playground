f8fa9c36a092979eefb5d469eb3958529e87960d
warmwhiten <ekekekgml@naver.com>
2022-06-06 03:23:39 +0900

    feat: set up environment for text input component

---
+++ packages/common-components/src/TextInput/TextInput.stories.tsx
@@ -0,0 +1,8 @@
+import TextInput from '.'
+
+export default {
+  title: 'Components/TextInput',
+  component: TextInput
+}
+
+export const Basic = (args: any) => <TextInput {...args} />
---
+++ packages/common-components/src/TextInput/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TextInput = () => {
+  return <input width="100px" height="100px" placeholder="input"></input>
+}
+
+export default TextInput
